4062f97123d788e78bb3914486cdc177a9e05c3b
Rgada28 <rgada28@gmail.com>
2019-01-23 19:35:22 +0530

    Added Admin page and saving page edited login page (dataBase)

--- online_banking/Login.aspx.cs
+++ online_banking/Login.aspx.cs
@@ -6,35 +6,48 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Data;
 
 public partial class Login : System.Web.UI.Page
 {
     public string conString = "Data Source=RAJGADA\\SQl;Initial Catalog=Bank_test;Integrated Security=True";
+    string str, UserName, Password;
     SqlConnection con = new SqlConnection();
     SqlCommand cmd = new SqlCommand();
     SqlDataReader rd;
-    protected void Page_Load(object sender, EventArgs e)
-    {
-
-    }
-
+    SqlDataAdapter sqlda;
+    DataTable dt;
+    int RowCount;
     protected void Login_tbn_Click(object sender, EventArgs e)
     {
         con.ConnectionString = conString;
         cmd.Connection = con;
         con.Open();
-        cmd.CommandText = "select * from userlog where Username='" + username_tb.Text + "'and Password='" + passwd_tb.Text + "'";
-        rd = cmd.ExecuteReader();
-        if (rd.HasRows)
+        cmd.CommandText = "select * from Login where Username='" + username_tb.Text + "'and Password='" + passwd_tb.Text + "'";
+        sqlda = new SqlDataAdapter(cmd.CommandText, con);
+        dt = new DataTable();
+        sqlda.Fill(dt);
+        RowCount = dt.Rows.Count;
+        for (int i = 0; i < RowCount; i++)
         {
-            Response.Redirect("~/HomeBeforeLogin.aspx");
+            UserName = dt.Rows[i]["UserName"].ToString();
+            Password = dt.Rows[i]["Password"].ToString();
+
+            if (UserName == username_tb.Text && Password == passwd_tb.Text)
+            {
+                Session["UserName"] = UserName;
+                if (dt.Rows[i]["Role"].ToString() == "Admin")
+                    Response.Redirect("~/Admin/Default.aspx");
+                else if (dt.Rows[i]["Role"].ToString() == "Saving")
+                    Response.Redirect("~/Saving/Default.aspx");
+                else if (dt.Rows[i]["Role"].ToString() == "Current")
+                    Response.Redirect("~/Saving/Default.aspx");
+                else if (dt.Rows[i]["Role"].ToString() == "Demat")
+                    Response.Redirect("~/Saving/Default.aspx");
+                else
+                    error_lbl.Text = "Invalid UserName or Password";
+            }
 
         }
-        else
-        {
-            error_lbl.Text = "Invalid Username/password!...";
-        }
-        con.Close();
-
     }
 }
---
+++ online_banking/Saving/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="Saving_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        &nbsp;<asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label1" runat="server" Text="Saving Account"></asp:Label>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
