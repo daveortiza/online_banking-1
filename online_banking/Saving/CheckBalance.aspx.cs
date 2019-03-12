01b495b64d5774a36fe28f4bf3caff566514057e
Rgada28 <rgada28@gmail.com>
2019-03-12 19:13:58 +0530

    Balance now can be viewed by check balance menu

--- online_banking/Saving/CheckBalance.aspx.cs
+++ online_banking/Saving/CheckBalance.aspx.cs
@@ -12,7 +12,7 @@ public partial class Saving_Transactions : System.Web.UI.Page
     string cs = ConfigurationManager.ConnectionStrings["Bank_test"].ConnectionString;
     protected void Page_Load(object sender, EventArgs e)
     {
-        Ac_bal.Text = "<Strong>Account Balance</Strong>:- "+Session["Account_Balance"];
+        Ac_bal.Text = "<Strong>Account Balance</Strong>:- "+Session["Account_Balance"]+".00 Cr";
         acno =Convert.ToInt32(Session["Account_Number"]);
         using (SqlConnection con = new SqlConnection(cs))
         {
